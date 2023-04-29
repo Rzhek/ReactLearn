d6fbf165440aed41f697eef92a99613992b96843
Rzhek <vladkurgankin2004@gmail.com>
2023-04-28 21:35:37 -0400

    basics of react done (day 4)

---
+++ react-http/src/App.js
@@ -0,0 +1,20 @@
+import logo from './logo.svg';
+import './App.css';
+
+import PostList from './Components/PostList';
+import PostForm from './Components/PostForm';
+
+function App() {
+    return (
+        <div className="App">
+            
+            
+
+            {/* <PostForm /> */}
+            {/* <PostList /> */}
+        </div>
+        );
+    }
+    
+    export default App;
+    
