1ef359b28bc6a539e1734ec7259cdc50f28f4860
Rzhek <vladkurgankin2004@gmail.com>
2023-04-28 22:38:23 -0400

    Hooks Intro (day 4)

---
+++ state-hook/src/App.js
@@ -0,0 +1,32 @@
+import logo from './logo.svg';
+import './App.css';
+import ClassCounter from './components/ClassCounter';
+import HookCounter from './components/HookCounter';
+import HookCounter2 from './components/HookCounter2';
+import HookCounter3 from './components/HookCounter3';
+import HookCounter4 from './components/HookCounter4';
+import HookCounter5 from './components/HookCounter5';
+import HookMouse from './components/HookMouse';
+import MouseContainer from './components/MouseContainer';
+
+
+
+function App() {
+    return (
+        <div className="App">
+
+            <MouseContainer />
+
+            {/* <HookMouse /> */}
+            {/* <HookCounter5 /> */}
+            {/* <HookCounter4 /> */}
+            {/* <HookCounter3 /> */}
+            {/* <HookCounter2 /> */}
+            {/* <ClassCounter /> */}
+            {/* <HookCounter /> */}
+        </div>
+        );
+    }
+    
+    export default App;
+    
